ebe3e6f2f6f1e9ee473106d1c64be3bc089ec85c
Ygor Mattos <ygormattos.b@gmail.com>
2021-03-24 20:47:23 -0300

    create more commands

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,8 +1,20 @@
+/**
+ * node index.js \
+ *  --username ygormattos \
+ *  --room sala01 \
+ *  --hostUri localhost
+ */
+
 import Events from 'events'
+import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
 
-const componentEmitter = new Events()
+const [, , ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+console.log(config)
 
-const controller = new TerminalController()
-await controller.initializeTable(componentEmitter)
+// const componentEmitter = new Events()
+
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
 
