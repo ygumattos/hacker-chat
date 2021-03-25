8db6b66ab2e357b25d22330f30c8f7448c1a3e5c
Ygor Mattos <ygormattos.b@gmail.com>
2021-03-24 21:30:34 -0300

    create connection

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -7,13 +7,16 @@
 
 import Events from 'events'
 import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 import TerminalController from "./src/terminalController.js";
 
 const [, , ...commands] = process.argv
 const config = CliConfig.parseArguments(commands)
-console.log(config)
 
-// const componentEmitter = new Events()
+const componentEmitter = new Events()
+
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
 
 // const controller = new TerminalController()
 // await controller.initializeTable(componentEmitter)
